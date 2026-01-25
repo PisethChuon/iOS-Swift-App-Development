23a20cab57d409f92587310190355a7957b510d7
PisethChuon <chuonpiseth05@gmail.com>
2026-01-25 23:09:00 +0700

    Fundamental of Swift

---
+++ Fundamentals of Swift 5.9.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+/*:
+ ## Classes
+ Classes are **reference** types. When you pass an instance to a variable or constant a reference to same instance is assigned. It is possible for multiple constants and variables to refer to the same instance of a class. This is known as reference counting.
+ 
+An instance of a class is created through *initialization*. Unlike structures, classes do not have memberwise initialization. This means that you need to have default values for all your properties or to at least provide the values at initialization in order to successfully create an instance of a class.
+*/
+
+//: [Next](@next)
+
+
+

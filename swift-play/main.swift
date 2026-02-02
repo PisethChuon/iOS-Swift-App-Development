c0650e054bab4ce183a1c2b3cc4c814f16132ac5
PisethChuon <chuonpiseth05@gmail.com>
2026-02-02 21:42:27 +0700

    Updated

--- swift-play/main.swift
+++ swift-play/main.swift
@@ -1,16 +1,4 @@
-enum LoginStatus: Int {
-    case success = 200
-    case failed = 401
-    case blocked = 403
+func sayHello() {
+    print("Hello, World!")
 }
-
-let status = LoginStatus.blocked
-
-switch status {
-case .success:
-    print("Login successful!" + " " + "\(status.rawValue)")
-case .failed:
-    print("Login failed." + " " + "\(status.rawValue)")
-case .blocked:
-    print("Account is blocked." + " " + "\(status.rawValue)")
-}
+sayHello()

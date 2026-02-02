4fb8456c30ffba3063656436a3f003a0e9f004ce
PisethChuon <chuonpiseth05@gmail.com>
2026-02-02 20:12:33 +0700

    Updated

--- swift-play/main.swift
+++ swift-play/main.swift
@@ -1,13 +1,16 @@
 enum LoginStatus {
     case success
     case failed
+    case blocked
 }
 
-let status = LoginStatus.success
+let status = LoginStatus.blocked
 
 switch status {
 case .success:
     print("Login successful!")
 case .failed:
     print("Login failed.")
+case .blocked:
+    print("Account is blocked.")
 }

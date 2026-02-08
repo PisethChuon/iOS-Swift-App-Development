f149c5adc2106262b509c7a6c840dbd4f69fbe2e
PisethChuon <chuonpiseth05@gmail.com>
2026-02-08 14:36:58 +0700

    IntroToSwiftUI

---
+++ introToSwiftUI/introToSwiftUI/introToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  introToSwiftUIApp.swift
+//  introToSwiftUI
+//
+//  Created by chuonpiseth on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct introToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

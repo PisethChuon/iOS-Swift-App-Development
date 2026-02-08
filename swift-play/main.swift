f6414ebfd66b31db7404a05a133d0675339df8ca
PisethChuon <chuonpiseth05@gmail.com>
2026-02-08 13:03:15 +0700

    Class

--- swift-play/main.swift
+++ swift-play/main.swift
@@ -1,18 +1,13 @@
-class Car {
-    var color: String
-    var speed: Int
-
-    init(color: String, speed: Int){
-        self.color = color
-        self.speed = speed
-    }
-
-    func drive() {
-        print ("The \(color) car is driving at \(speed) km/h")
+class Animal {
+    func makeSound() {
+        print("Animal makes a sound")
     }
 }
 
-var myCar = Car(color: "red", speed: 120)
-var anotherCar = myCar
-anotherCar.color = "blue"
-myCar.drive() // Output: The blue car is driving at 120 km/h
+class Dog: Animal {
+    override func makeSound() {
+        print("Dog barks")
+    }
+}
+
+Dog().makeSound() // Output: Dog barks

59cb942e889f3d7f31ed8a8283e195c1d1b9b636
PisethChuon <chuonpiseth05@gmail.com>
2026-02-10 23:51:21 +0700

    Layout and view

--- viewAndLayout/viewAndLayout/ContentView.swift
+++ viewAndLayout/viewAndLayout/ContentView.swift
@@ -9,27 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            Color.gray
-                .ignoresSafeArea()
-
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Title at the top")
-                    .font(.title3)
-                    .bold()
-
-                Text("Description")
-                    .font(.subheadline)
+        VStack {
+            Text("Hello")
+            Text("Welcome")
+            
+            HStack {
+                Image(systemName: "person.fill")
+                Text("Person")
             }
-            .padding()
-
-            Text("NEW")
-                .font(.caption)
-                .padding(6)
-                .background(Color.red)
-                .foregroundColor(.white)
-                .cornerRadius(6)
-                .padding()
+            
+            ZStack {
+                Color.gray
+            }
+            
         }
     }
 }

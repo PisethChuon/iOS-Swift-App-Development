26034854105f5c2429310a572da306a92f633f1c
PisethChuon <chuonpiseth05@gmail.com>
2026-02-12 23:04:55 +0700

    Update

--- viewAndLayout/viewAndLayout/ContentView.swift
+++ viewAndLayout/viewAndLayout/ContentView.swift
@@ -10,6 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            GeometryReader { geometry in
+                VStack {
+                    Color.white
+                        .frame(width: geometry.size.width * 0.8, height: 300)
+                }
+                .frame(maxWidth: .infinity) // Centers the content
+            }
+            
+            
+                
             
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)

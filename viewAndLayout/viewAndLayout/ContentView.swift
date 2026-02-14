d95bca088e9b0c4fec21769f8cb7f47cc755cc13
PisethChuon <chuonpiseth05@gmail.com>
2026-02-14 21:01:46 +0700

    Update

--- viewAndLayout/viewAndLayout/ContentView.swift
+++ viewAndLayout/viewAndLayout/ContentView.swift
@@ -9,11 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var isLiked = false
+    let name: String = "Piseth CHUON"
 
     var body: some View {
-        VStack {
-            Image(systemName: "trash")
-                .accessibilityLabel("Delete")
+        
+        VStack(spacing: 16) {
+            Image(systemName: "person.circle.fill")
+                .font(.title)
+            Text(name)
+            
+            HStack(spacing: 24) {
+                Button {
+                    withAnimation {
+                        isLiked.toggle()
+                    }
+                } label: {
+                    Image(systemName: "heart")
+                        .symbolVariant(isLiked ? .fill : .none)
+                }
+                Image(systemName: "message")
+                    .foregroundStyle(.gray)
+                Image(systemName: "square.and.arrow.up")
+                    .foregroundStyle(.gray)
+            }
+            .font(.title)
         }
     }
 }

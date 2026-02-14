45438e926871e746da43c34667de0125cbac9cdb
PisethChuon <chuonpiseth05@gmail.com>
2026-02-14 13:46:30 +0700

    Profile header

--- viewAndLayout/viewAndLayout/ContentView.swift
+++ viewAndLayout/viewAndLayout/ContentView.swift
@@ -9,20 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        let name: String = "Piseth Chuon"
-        let job: String = "iOS Developer"
-        let status: Int? = 1
-        
-        VStack {
-            Text("\(name)")
-                .font(.largeTitle)
-                .bold()
-            Text("\(job)")
-                .font(.subheadline)
-                .foregroundStyle(.gray)
-            Text(status == 1 ? "Online" : "Offline")
-                .foregroundStyle(status == 1 ? .green : .red)
-                .italic()
+        Button {
+            print("Tapped!")
+        } label: {
+            Label("Send", systemImage: "paperplane.fill")
         }
     }
 }

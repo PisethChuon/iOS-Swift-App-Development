475a266bd2fa19d86b500362c88c8a7b8bb3f41b
PisethChuon <chuonpiseth05@gmail.com>
2026-02-14 12:42:00 +0700

    Profile header

--- viewAndLayout/viewAndLayout/ContentView.swift
+++ viewAndLayout/viewAndLayout/ContentView.swift
@@ -11,9 +11,13 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Text("This is a block of text that will show up in a text element as multiple lines. The text will fill the available space, and then, eventually, be truncated.")
+            Text("Chuon Piseth")
+                .font(Font.largeTitle)
+                .bold()
+            Text("iOS Developer at ABA")
+                .foregroundStyle(.gray)
                 .lineLimit(1)
-                .minimumScaleFactor(4.0)
+                .truncationMode(.tail)
         }
     }
 }

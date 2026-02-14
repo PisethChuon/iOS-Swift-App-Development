b6fbe1e7850e4d3bafef35edc152894856878e3e
PisethChuon <chuonpiseth05@gmail.com>
2026-02-15 01:02:45 +0700

    Color

--- viewAndLayout/viewAndLayout/ContentView.swift
+++ viewAndLayout/viewAndLayout/ContentView.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-        
+    
     var body: some View {
-        
-
+        VStack {
+            Color.primaryTheme
+            
+        }
         
     }
-        
 }
 
 //struct Triangle: Shape {

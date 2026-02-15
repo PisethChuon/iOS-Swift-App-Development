c243d40a528f444dd89a5ded3c6f1a780a1389c9
PisethChuon <chuonpiseth05@gmail.com>
2026-02-15 14:59:49 +0700

    State

--- viewAndLayout/viewAndLayout/ContentView.swift
+++ viewAndLayout/viewAndLayout/ContentView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var count = 0
     
     var body: some View {
         VStack {
+            Text("Count: \(count)")
+                .font(Font.largeTitle)
+            Button("Increase") {
+                count += 1
+            }
         }
         
     }

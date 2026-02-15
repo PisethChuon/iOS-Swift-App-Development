a1eeb449fab20d79ffd73661d055107e15835caf
PisethChuon <chuonpiseth05@gmail.com>
2026-02-15 23:33:03 +0700

    TextField

--- viewAndLayout/viewAndLayout/ContentView.swift
+++ viewAndLayout/viewAndLayout/ContentView.swift
@@ -1,34 +1,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var username: String = ""
-    @State private var isPremium: Bool = false
     
     var body: some View {
         VStack {
-            ProfileForm(username: $username, isPremium: $isPremium)
+            TextField("Username", text: $username)
+                .font(.system(size: 20, weight: .semibold))
+                .keyboardType(.alphabet)
+                .textContentType(.username)
+                .padding(.horizontal, 30)
+                .textFieldStyle(.roundedBorder)
         }
     }
 }
 
-// Chill
-struct ProfileForm: View {
-
-    @Binding var username: String
-    @Binding var isPremium: Bool
-
-    var body: some View {
-        
-        TextField("Enter username", text: $username)
-            .textFieldStyle(.roundedBorder)
-        
-        Text("Hello, \(username)")
-            .padding()
-            .background(Color.blue)
-        Toggle("Premium", isOn: $isPremium)
-    }
-}
 
 #Preview {
     ContentView()

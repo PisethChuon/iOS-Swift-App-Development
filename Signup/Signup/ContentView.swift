0d9339a515da646a35f46c63672fb08ec2bdf8fa
PisethChuon <chuonpiseth05@gmail.com>
2026-02-16 22:32:55 +0700

    Signup

---
+++ Signup/Signup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Signup
+//
+//  Created by chuonpiseth on 16/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Signup/Signup/SignupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignupApp.swift
+//  Signup
+//
+//  Created by chuonpiseth on 16/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SignupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

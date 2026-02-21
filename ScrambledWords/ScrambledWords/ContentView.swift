e5d0a30096a88eb74b95a119f539e9ebf1159c48
PisethChuon <chuonpiseth05@gmail.com>
2026-02-21 22:34:21 +0700

    Scramble Game

--- ScrambledWords/ScrambledWords/ContentView.swift
+++ ScrambledWords/ScrambledWords/ContentView.swift
@@ -13,14 +13,36 @@ struct ContentView: View {
             ZStack {
                 Color.background.ignoresSafeArea()
                 VStack {
-                    Image("orange")
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                }
-                .frame(width: proxy.size.width * 0.8)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.border, lineWidth: 2)
+                    VStack {
+                        Spacer()
+                        Image("orange")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                        Spacer()
+                        HStack {
+                            VStack {
+                                LetterView(character: "")
+                                Rectangle()
+                                    .fill(Color.white)
+                                    .frame(width: 20, height: 2)
+                            }
+                        }
+                        .padding(.bottom, 20)
+                    }
+                    .frame(width: proxy.size.width * 0.8, height: proxy.size.width * 0.9)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.border, lineWidth: 2)
+                    }
+                    Text("Score: 0")
+                        .foregroundStyle(Color.white)
+                        .font(.system(size: 24))
+                        .padding(.top)
+                    HStack {
+                        LetterView(character: "O")
+                        LetterView(character: "R")
+                        LetterView(character: "A")
+                    }
                 }
             }
         }
@@ -29,4 +51,16 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct LetterView: View {
+    let character: String
+    var body: some View {
+        Text(character)
+            .font(.system(size: 30, weight: .semibold))
+            .foregroundStyle(Color.white)
+            .frame(width: 30, height: 30)
+            .background(Color.white.opacity(0.4))
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
 }

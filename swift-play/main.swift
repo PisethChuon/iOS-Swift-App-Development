8428c4ac3b3844da85c1ed1e216b432c0f1d34c6
PisethChuon <chuonpiseth05@gmail.com>
2026-02-22 22:13:46 +0700

    Protocol

--- swift-play/main.swift
+++ swift-play/main.swift
@@ -1,14 +1,3 @@
-protocol Drivable {
-    func startEngine()
-    func stopEngine()
-}
-
-struct Car: Drivable {
-    func startEngine() {
-        print("Engine started")
-    }
-
-    func stopEngine() {
-        print("Engine stopped")
-    }
+protocol Animal {
+    func makeSound() 
 }

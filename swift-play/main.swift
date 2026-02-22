09fff44dedf523b109e722be90f9dc6140cde78b
PisethChuon <chuonpiseth05@gmail.com>
2026-02-22 23:49:30 +0700

    Protocol

--- swift-play/main.swift
+++ swift-play/main.swift
@@ -1,3 +1,9 @@
-protocol Animal {
-    func makeSound() 
+enum AppState: Equatable {
+    case loading
+    case success(message: String)
 }
+
+let appState1 = AppState.success(message: "Done")
+let appState2 = AppState.success(message: "Done")
+
+print(appState1 == appState2)

017eb09e05d8aef5a1e4cdd76ee0111d4c629d8c
PisethChuon <chuonpiseth05@gmail.com>
2026-02-26 00:02:06 +0700

    Student List

---
+++ Student List/Student List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Student List
+//
+//  Created by chuonpiseth on 25/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Student List/Student List/Student_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Student_ListApp.swift
+//  Student List
+//
+//  Created by chuonpiseth on 25/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Student_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

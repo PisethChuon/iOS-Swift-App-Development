4d4623944512ab3966e434e242f30c0995164ec2
PisethChuon <chuonpiseth05@gmail.com>
2026-03-07 17:08:53 +0700

    SheetsAndNavigation

---
+++ SheetsAndNavigation/SheetsAndNavigation/SheetsAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetsAndNavigationApp.swift
+//  SheetsAndNavigation
+//
+//  Created by chuonpiseth on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SheetsAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

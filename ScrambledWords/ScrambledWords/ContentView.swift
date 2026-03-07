90211508c5b5778dda29c1a9bb8dffa07a86cde5
PisethChuon <chuonpiseth05@gmail.com>
2026-03-07 15:46:08 +0700

    Alert

--- ScrambledWords/ScrambledWords/ContentView.swift
+++ ScrambledWords/ScrambledWords/ContentView.swift
@@ -106,6 +106,7 @@ struct ContentView: View {
                                 }
                             }
                             guessedLetters.removeAll()
+                            showResult = false
                         }
                     }
                 }

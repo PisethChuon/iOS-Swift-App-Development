44154498d94f09147d0f05e621224bf5fd3e76da
PisethChuon <chuonpiseth05@gmail.com>
2026-03-11 07:12:30 +0700

    SheetsAndNavigation

--- SheetsAndNavigation/SheetsAndNavigation/ContentView.swift
+++ SheetsAndNavigation/SheetsAndNavigation/ContentView.swift
@@ -7,36 +7,17 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    let id = UUID()
-    let name: String
-}
 
 struct ContentView: View {
     
     @State private var selectedUser: User? = nil
     
     var body: some View {
-        VStack (spacing:20) {
-            Button ("Show item") {
-                selectedUser = User(name: "Piseth")
-            }
-        }
-        .sheet(item: $selectedUser, onDismiss: {
-            print("Sheet closed")
-        }) {
-            user in
-            VStack {
-                Text("Hello \(user.name)")
-                
-                Button("Close") {
-                    selectedUser = nil
-                }
-            }
+        
+        
         }
     }
         
-}
 
 #Preview {
     ContentView()

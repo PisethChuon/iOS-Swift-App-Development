7566779c5e8f496ec79fa19a1e971eecf76a780e
PisethChuon <chuonpiseth05@gmail.com>
2026-03-16 23:16:17 +0700

    SimpleMVVM

--- SimpleMVVM/SimpleMVVM/CounterDisplayView.swift
+++ SimpleMVVM/SimpleMVVM/CounterDisplayView.swift
@@ -5,3 +5,18 @@
 //  Created by chuonpiseth on 16/3/26.
 //
 
+import SwiftUI
+//import Combine
+
+struct CounterDisplayView: View {
+    
+    @ObservableObject var vm: CounterViewModel
+    
+    var body: some View {
+        Text("Current view: \(vm.count)")
+    }
+}
+
+#Preview {
+    CounterDisplayView()
+}

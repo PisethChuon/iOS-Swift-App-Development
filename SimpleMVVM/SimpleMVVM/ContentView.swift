31fd954d7d7721b69de9bccc7d9316811eb7f3ce
PisethChuon <chuonpiseth05@gmail.com>
2026-03-16 23:06:46 +0700

    SimpleMVVM

--- SimpleMVVM/SimpleMVVM/ContentView.swift
+++ SimpleMVVM/SimpleMVVM/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var vm = CounterViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Count: \(vm.count)")
+            Button("Increase") {
+                vm.increase()
+            }
         }
-        .padding()
     }
 }
 

487618ec9343965bcb99cf016330e94e992883af
PisethChuon <chuonpiseth05@gmail.com>
2026-03-19 21:34:23 +0700

    List

---
+++ List/List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  List
+//
+//  Created by chuonpiseth on 19/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

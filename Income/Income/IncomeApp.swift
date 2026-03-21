acbae5a6fff8bc1afb8ef7d531f64dd3719d25b7
PisethChuon <chuonpiseth05@gmail.com>
2026-03-21 15:21:20 +0700

    Init commit

---
+++ Income/Income/IncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp.swift
+//  Income
+//
+//  Created by chuonpiseth on 21/3/26.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

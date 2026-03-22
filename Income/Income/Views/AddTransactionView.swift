977b50ef03382d862343f22cf62f8dee191de89e
PisethChuon <chuonpiseth05@gmail.com>
2026-03-22 15:36:55 +0700

    feat: using a picker to express transaction type option

--- Income/Income/Views/AddTransactionView.swift
+++ Income/Income/Views/AddTransactionView.swift
@@ -10,11 +10,14 @@ import SwiftUI
 struct AddTransactionView: View {
     
     @State private var amount = 0.0
+    @State private var selectedTransactionType: TransactionType = .expense
+    
     var numberFormatter: NumberFormatter {
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .currency
         return numberFormatter
     }
+    
     
     var body: some View {
         VStack {
@@ -22,6 +25,12 @@ struct AddTransactionView: View {
                 .font(.system(size: 60, weight: .thin))
                 .multilineTextAlignment(.center)
                 .keyboardType(.numberPad)
+            Picker("Choose Type", selection: $selectedTransactionType) {
+                ForEach(TransactionType.allCases) {transactionType in
+                    Text(transactionType.title)
+                        .tag(transactionType)
+                }
+            }
             Spacer()
         }
     }

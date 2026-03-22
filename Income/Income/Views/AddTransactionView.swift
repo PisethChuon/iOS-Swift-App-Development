7a7d6fbfa251b739f9223e735fce75840de2ba39
PisethChuon <chuonpiseth05@gmail.com>
2026-03-22 22:12:34 +0700

    feat: add text title

--- Income/Income/Views/AddTransactionView.swift
+++ Income/Income/Views/AddTransactionView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AddTransactionView: View {
     
     @State private var amount = 0.0
+    @State private var transactionTtile = ""
     @State private var selectedTransactionType: TransactionType = .expense
     
     var numberFormatter: NumberFormatter {
@@ -31,6 +32,11 @@ struct AddTransactionView: View {
                         .tag(transactionType)
                 }
             }
+            TextField("Title", text: $transactionTtile)
+                .font(.system(size: 15))
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal, 30)
+                .padding(.top)
             Spacer()
         }
     }

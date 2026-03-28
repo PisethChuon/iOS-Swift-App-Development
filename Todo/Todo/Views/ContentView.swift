f513925a4c3d0a3ffe2f43d732a0d802026d90d4
PisethChuon <chuonpiseth05@gmail.com>
2026-03-28 15:57:33 +0700

    define Task model

--- Todo/Todo/Views/ContentView.swift
+++ Todo/Todo/Views/ContentView.swift
@@ -7,15 +7,24 @@
 
 import SwiftUI
 
+// Model 'Task'
+struct Task: Identifiable {
+    let id = UUID()
+    var title: String
+}
+
 struct ContentView: View {
     
     @State private var showSheet = false
+    @State private var multiSelection = Set<UUID>()
     
     var body: some View {
         NavigationStack {
-            List {
-                
+            List (selection: $multiSelection) {
+                TaskView()
             }
+            Text("\(multiSelection.count) selection")
+            
                 .toolbar {
                     ToolbarItem(placement: .topBarTrailing) {
                         Button {

a3340b22fcf267ba7678db6f36e8a8fe0c63549b
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-03-29 21:59:38 +0700

    Add setting view

--- Income/Income/Views/SettingsView.swift
+++ Income/Income/Views/SettingsView.swift
@@ -10,13 +10,21 @@ import SwiftUI
 struct SettingsView: View {
     
     @State private var orderDescending = false
-//    @State private var currency: Currency = .usd
+    @State private var currency: Currency = .usd
     
     var body: some View {
         List {
             HStack {
                 Toggle(isOn: $orderDescending) {
                     Text("Order \(orderDescending ? "(Earliest)" : "(Latest)")")
+                }
+            }
+            HStack {
+                Picker("Currency", selection: $currency) {
+                    ForEach (Currency.allCases, id: \.self) {
+                        currency in
+                        Text(currency.title)
+                    }
                 }
             }
         }

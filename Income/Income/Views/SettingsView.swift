081bced882a6b3330cd57e13eeafad21c31c54d8
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-03-29 21:47:06 +0700

    Add setting view

---
+++ Income/Income/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Income
+//
+//  Created by chuonpiseth on 29/3/26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    SettingsView()
+}

4ef5757b22367ec18c5df1346c7e44f0acdd78ba
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-04-05 19:54:25 +0700

    TextField

--- FavoriteFruits/FavoriteFruits/ContentView.swift
+++ FavoriteFruits/FavoriteFruits/ContentView.swift
@@ -9,10 +9,26 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var fruitName = ""
+    @State private var quantity = ""
+    
     var body: some View {
-        
+        VStack(spacing: 20) {
+            TextField("Fruit name", text: $fruitName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Quantity", text: $quantity)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Add Fruit") {
+                
+            }
+        }
+        .padding()
     }
+    
+    
 }
 
 

3350215fa6e284bf86b3ce03b08979f1287d62a5
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-04-11 15:16:52 +0700

    call share data

--- Income Starter/Income/IncomeApp.swift
+++ Income Starter/Income/IncomeApp.swift
@@ -8,9 +8,13 @@ import SwiftUI
 
 @main
 struct IncomeApp: App {
+    
+    let dataManager = DataManager.shared
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
+                .environment(\.managedObjectContext, dataManager.container.viewContext)
         }
     }
 }

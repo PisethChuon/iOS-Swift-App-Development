250141e8ddae975c687c68c4b0a818a6ac131724
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-04-25 15:53:37 +0700

    Show ui

--- Task/Task/ContentView.swift
+++ Task/Task/ContentView.swift
@@ -11,12 +11,26 @@ import CoreData
 struct ContentView: View {
     
     @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \TaskItem.createdAt, ascending: true)])
+    private var tasks: FetchedResults<TaskItem>
     
     var body: some View {
         Button("Add Task") {
-            let newTask = Task(context: viewContext)
-//            newTask.title = "My first task"
-//            newTask.createdAt = Date()
+            let newTask = TaskItem(context: viewContext)
+            newTask.title = "My first task"
+            newTask.createdAt = Date()
+            
+            do {
+                try viewContext.save()
+                print("Saved!")
+            } catch {
+                print("Error saving")
+            }
+        }
+        List {
+            ForEach(tasks) { task in
+                Text(task.title ?? "No Title")
+            }
         }
     }
     

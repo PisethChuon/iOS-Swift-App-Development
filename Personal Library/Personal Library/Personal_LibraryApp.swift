a112db5233ba9bbe3a33909230f2e8ee7640a2fd
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-01 14:01:01 +0700

    SwiftData

---
+++ Personal Library/Personal Library/Personal_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_LibraryApp.swift
+//  Personal Library
+//
+//  Created by chuonpiseth on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Personal_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

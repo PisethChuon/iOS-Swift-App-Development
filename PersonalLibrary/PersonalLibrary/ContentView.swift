4080efb97112a29d86c370c292d4c63e288211a8
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-02 15:46:47 +0700

    Personal Library

--- PersonalLibrary/PersonalLibrary/ContentView.swift
+++ PersonalLibrary/PersonalLibrary/ContentView.swift
@@ -1,1 +1,13 @@
+import SwiftUI
+import SwiftData
 
+struct ContentView: View {
+    @Environment(\.modelContext) private var context
+    
+    var body: some View {
+        Button("Add Test Book") {
+            let book = Book(title: "Dune", author: "Frank Herbert")
+            context.insert(book)    // Write to DB
+        }
+    }
+}

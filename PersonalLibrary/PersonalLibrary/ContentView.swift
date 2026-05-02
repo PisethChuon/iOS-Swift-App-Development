4079d500898c7ab5468dca5e96cce69716983087
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-02 22:49:31 +0700

    Inser database

--- PersonalLibrary/PersonalLibrary/ContentView.swift
+++ PersonalLibrary/PersonalLibrary/ContentView.swift
@@ -3,8 +3,17 @@ import SwiftData
 
 struct ContentView: View {
     
+    @Environment(\.modelContext) private var context
     
     var body: some View {
+    }
+    
+    func addBook() {
+        let book = Book(
+            title: "Book \(Int.random(in: 0...100))",
+            author: "Author"
+        )
+        context.insert(book)
     }
     
 }

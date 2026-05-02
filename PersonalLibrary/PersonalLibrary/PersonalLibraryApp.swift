7a31c4722024675f5f0804cb8900e6ab625925a7
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-02 15:41:49 +0700

    Personal Library

--- PersonalLibrary/PersonalLibrary/PersonalLibraryApp.swift
+++ PersonalLibrary/PersonalLibrary/PersonalLibraryApp.swift
@@ -1,1 +1,12 @@
+import SwiftUI
+import SwiftData
 
+@main
+struct PersonalLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Book.self) // Create the DB
+    }
+}

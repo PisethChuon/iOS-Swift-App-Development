51671d4157b38850c40883333c5dadb58eaeaf33
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-03 23:07:16 +0700

    fix crush app

--- ScoreKeeper/ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var players: [String] = ["Elisha", "Andre", "Jasmine"]
+    @State private var score: [Int] = [0, 0, 0]
     
     var body: some View {
         VStack {
             ForEach(0..<players.count, id: \.description) { index in
                 TextField("Name", text: $players[index])
+                Stepper("\(score[index])", value: $score[index])
             }
             Button("Add Player", systemImage: "plus") {
                 players.append("")

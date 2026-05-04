6c1cd995e1ac9d530b6c486cbf2e12820f2ae6b1
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-04 14:00:29 +0700

    Unit test

--- ScoreKeeper/ScoreKeeper/Scoreboard.swift
+++ ScoreKeeper/ScoreKeeper/Scoreboard.swift
@@ -5,3 +5,13 @@
 //  Created by chuonpiseth on 4/5/26.
 //
 
+import Foundation
+
+struct Scoreboard {
+    var players: [Player] = [
+        Player(name: "Elisha", score: 0),
+        Player(name: "Andre", score: 0),
+        Player(name: "Jasmine", score: 0),
+    ]
+    
+}

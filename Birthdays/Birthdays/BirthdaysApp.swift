9fd2263ba0d6e12b4c24a18931ed42c37b1a9dbc
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-05 11:37:11 +0700

    Birthdays

---
+++ Birthdays/Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by chuonpiseth on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

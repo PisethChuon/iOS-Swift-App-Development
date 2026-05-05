e20c9dcaba3fa38a80ef04778de6383b5c9da649
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-05 12:53:32 +0700

    Enter data

--- Birthdays/Birthdays/ContentView.swift
+++ Birthdays/Birthdays/ContentView.swift
@@ -13,6 +13,9 @@ struct ContentView: View {
         Friend(name: "Piseth", birthday: Date(timeIntervalSince1970: 0))
     ]
     
+    @State private var newName = ""
+    @State private var newDate = Date.now
+    
     var body: some View {
         NavigationStack {
             List(friends, id: \.name) { friend in
@@ -21,6 +24,27 @@ struct ContentView: View {
                     Spacer()
                     Text(friend.birthday, format: .dateTime.month(.wide).day().year())
                 }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newDate, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newDate)
+                        friends.append(newFriend)
+                        
+                        newName = ""
+                        newDate = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
             }
         }
     }

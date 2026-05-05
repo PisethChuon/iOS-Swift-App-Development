da919c7f2061de94cb72f4d1c81a6f3dafc45667
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-06 00:12:10 +0700

    save data to device

--- GroceryList/GroceryList/ContentView.swift
+++ GroceryList/GroceryList/ContentView.swift
@@ -6,19 +6,53 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Query private var groceryLists: [GroceryList]
+    @Environment(\.modelContext) private var context
+    
+    @State private var itemName = ""
+    @State private var isCart: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, grocery!")
+        NavigationStack {
+            List(groceryLists) { groceryList in
+                HStack {
+                    Text("\(groceryList.items)")
+                        .bold()
+                    Spacer()
+                    
+                    Toggle("", isOn: $isCart)
+                }
+            }
+            
+            .navigationTitle("Grocery List")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Item")
+                        .font(.headline)
+                    TextField("Item Name", text: $itemName)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal, 100)
+                    Button("Add item") {
+                        let newItem = GroceryList(items: itemName)
+                        context.insert(newItem)
+                        
+                        itemName = ""
+                    }
+                }
+                .padding()
+                .background(.bar)
+            }
+            
         }
-        .padding()
+        
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: GroceryList.self, inMemory: true)
 }

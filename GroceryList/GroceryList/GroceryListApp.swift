317ba98e33da99a241f1756ab4f57ebe65309e7f
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-05 22:24:15 +0700

    GroceryList

---
+++ GroceryList/GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by chuonpiseth on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

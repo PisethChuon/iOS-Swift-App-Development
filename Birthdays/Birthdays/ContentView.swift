67c6907ca5886ce0a57cbbf5ef248f35e31df1ea
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-05 16:10:19 +0700

    update sort

--- Birthdays/Birthdays/ContentView.swift
+++ Birthdays/Birthdays/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Query(sort: [SortDescriptor(\Friend.birthday), SortDescriptor(\Friend.name)]) private var friends: [Friend]
+    @Query(sort: \Friend.name) private var friends: [Friend]
     @Environment(\.modelContext) private var context
     
     @State private var newName = ""

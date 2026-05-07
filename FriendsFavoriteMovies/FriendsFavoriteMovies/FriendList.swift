dbbaf1a92d232a5703388e9ae7491296c798d0e8
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-07 23:02:56 +0700

    FriendList View

--- FriendsFavoriteMovies/FriendsFavoriteMovies/FriendList.swift
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FriendList.swift
@@ -5,3 +5,19 @@
 //  Created by chuonpiseth on 7/5/26.
 //
 
+import SwiftUI
+import SwiftData
+
+struct FriendList: View {
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
+    
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    FriendList()
+        .modelContainer(for: Friend.self, inMemory: true)
+}

8573a620984d868634953fbc55d0fded05e5dc6d
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-07 22:45:54 +0700

    FriendsFavoriteMovies

---
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by chuonpiseth on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

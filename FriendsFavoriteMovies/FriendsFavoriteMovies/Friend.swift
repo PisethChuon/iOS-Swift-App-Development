c3b3f900ee0ccadfc84de242271b41d30685d2ac
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-07 22:55:32 +0700

    Model Friend

--- FriendsFavoriteMovies/FriendsFavoriteMovies/Friend.swift
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/Friend.swift
@@ -5,3 +5,14 @@
 //  Created by chuonpiseth on 7/5/26.
 //
 
+import Foundation
+import SwiftData
+
+@Model
+class Friend {
+    var name: String
+    
+    init (name: String) {
+        self.name = name
+    }
+}

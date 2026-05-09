ff4411d31661671009b5f13c74c11a1f9b29be24
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-09 22:01:25 +0700

    Movie list

--- FriendsFavoriteMovies/FriendsFavoriteMovies/MovieList.swift
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/MovieList.swift
@@ -5,11 +5,25 @@
 //  Created by chuonpiseth on 9/5/26.
 //
 
-import Foundation
+import SwiftUI
 import SwiftData
 
-struct MovieList: Decodable {
+struct MovieList: View {
+    @Query private var movies: [Movie]
+    @Environment(\.modelContext) private var context
     
-    
-    
+    var body: some View {
+        List {
+            ForEach(movies) { movie in
+                Text(movie.title)
+//                Text(movie.releaseDate)
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    FriendList()
+        .modelContainer(SampleData.shared.modelContainer)
 }

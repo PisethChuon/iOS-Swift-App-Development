6af4eeee4c557e15015603f283600990ca73c439
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-09 22:03:48 +0700

    Call 'MovieList()'

--- FriendsFavoriteMovies/FriendsFavoriteMovies/ContentView.swift
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
             }
             
             Tab("Movie", systemImage: "film.stack") {
-                Text("Movies")
+                MovieList()
             }
         }
     }

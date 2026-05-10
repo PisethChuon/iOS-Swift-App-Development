7d1e4e98270b59116178f45cafea68bf9e405cdb
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-11 00:02:40 +0700

    shee view for friend name

--- FriendsFavoriteMovies/FriendsFavoriteMovies/FriendList.swift
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FriendList.swift
@@ -11,6 +11,7 @@ import SwiftData
 struct FriendList: View {
     @Query private var friends: [Friend]
     @Environment(\.modelContext) private var context
+    @State private var newFriend: Friend?
     
     var body: some View {
         NavigationSplitView {
@@ -33,6 +34,11 @@ struct FriendList: View {
                     EditButton()
                 }
             }
+            .sheet(item: $newFriend) { friend in
+                NavigationStack {
+                    FriendDetail(friend: friend)
+                }
+            }
         } detail: {
             Text("Select a friend")
                 .navigationTitle("Friend")
@@ -42,7 +48,9 @@ struct FriendList: View {
     }
     
     private func addFriend() {
-        context.insert(Friend(name: "New friend "))
+        let newFriend = Friend(name: "")
+        context.insert(newFriend)
+        self.newFriend = newFriend
     }
     
     private func deleteFriend(indexes: IndexSet) {

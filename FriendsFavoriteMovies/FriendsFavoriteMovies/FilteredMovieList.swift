447aae9f2862293bba84a412c7d1d27a33717bf2
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-17 13:32:17 +0700

    FilteredMovieList

---
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FilteredMovieList.swift
@@ -0,0 +1,14 @@
+//
+//  FilteredMovieList.swift
+//  FriendsFavoriteMovies
+//
+//  Created by chuonpiseth on 17/5/26.
+//
+
+import SwiftUI
+
+struct FilteredMovieList: View {
+    var body: some View {
+        MovieList()
+    }
+}

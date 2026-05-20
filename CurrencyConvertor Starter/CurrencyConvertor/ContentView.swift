3ad8dd65068b035937124f4f3fab2136517dd765
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-20 22:51:27 +0700

    Basic UI

--- CurrencyConvertor Starter/CurrencyConvertor/ContentView.swift
+++ CurrencyConvertor Starter/CurrencyConvertor/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text("Amount")
+                .font(.system(size: 15))
             
         }
     }

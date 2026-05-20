16c5c891f6fc7c00b70a6a68ea7cf28e25520e10
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-20 22:53:39 +0700

    Basic UI

--- CurrencyConvertor Starter/CurrencyConvertor/ContentView.swift
+++ CurrencyConvertor Starter/CurrencyConvertor/ContentView.swift
@@ -8,11 +8,19 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var amount: String = ""
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("Amount")
                 .font(.system(size: 15))
-            
+            TextField("", text: $amount)
+                .font(.system(size: 18, weight: .semibold))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(.clear)
+                        .stroke(Color.gray, lineWidth: 1)
+                }
         }
     }
 }

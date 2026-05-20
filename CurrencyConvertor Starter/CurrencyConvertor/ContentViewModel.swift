2459e438801c041aa8bc5855130c7c5b342cdce3
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-21 06:48:49 +0700

    View Model

---
+++ CurrencyConvertor Starter/CurrencyConvertor/ContentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentViewModel.swift
+//  CurrencyConvertor
+//
+//  Created by chuonpiseth on 21/5/26.
+//
+
+import Foundation
+
+class ContentViewModel: ObservableObject {
+    @Published var convertedAmount = ""
+}

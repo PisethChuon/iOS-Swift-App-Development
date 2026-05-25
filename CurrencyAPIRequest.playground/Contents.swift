2da3a913d68a386b7c84892bbb0cec5e00ef9e5e
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-26 06:42:11 +0700

    API request using compeletion handler

--- CurrencyAPIRequest.playground/Contents.swift
+++ CurrencyAPIRequest.playground/Contents.swift
@@ -3,3 +3,14 @@ import UIKit
 let baseURL = "https://openexchangerates.org/api/latest.json?app_id=a921f26b655c4b4597b4d60d4106ed50"
 
 let url = URL(string: baseURL)!
+
+let urlRequest = URLRequest(url: url)
+
+URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+        if let error = error {
+            print(error.localizedDescription)
+        return
+    }
+    
+    
+}

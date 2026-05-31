afcf78d7e58e6d51a785e4fd0960f8004e76e53a
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-31 18:24:58 +0700

    Codable

--- findout/BasicApi.playground/Contents.swift
+++ findout/BasicApi.playground/Contents.swift
@@ -8,8 +8,22 @@ struct User: Codable {
 
 let json = """
 {
- "id": 1,
-"name": "Leanne Graham",
-"email": "leanne@example.com"   
+    "id": 1,
+    "name": "Leanne Graham",
+    "email": "leanne@example.com"   
 }
 """.data(using: .utf8)!
+
+do {
+    let user = try JSONDecoder().decode(
+        User.self,
+        from: json
+    )
+    
+    print(user.id)
+    print(user.name)
+    print(user.email)
+    
+} catch {
+    print(error)
+}

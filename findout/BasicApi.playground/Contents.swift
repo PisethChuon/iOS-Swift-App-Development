4e0ae4924fa4c6aa7908a23f12a0231fc3c43fba
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-31 16:31:01 +0700

    create data

--- findout/BasicApi.playground/Contents.swift
+++ findout/BasicApi.playground/Contents.swift
@@ -1,3 +1,17 @@
 import Foundation
 
 let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+let task = URLSession.shared.dataTask(with: url) { data, response, error in
+    
+    if let error = error {
+        print("Error: \(error)")
+        return
+    }
+    
+    guard let data = data else {
+        print("No data received")
+        return
+    }
+    
+    
+}

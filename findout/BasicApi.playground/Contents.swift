c18c03d37a13b5947b24856cc449935e9cffed9a
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-05-31 18:54:27 +0700

    Codable

--- findout/BasicApi.playground/Contents.swift
+++ findout/BasicApi.playground/Contents.swift
@@ -3,30 +3,33 @@ import Foundation
 struct User: Codable {
     let id: Int
     let name: String
-    let email: String
 }
 
 let json = """
 
-{
-
+[
+  {
     "id": 1,
-
-    "name": "Leanne Graham",
-
-    "email": "leanne@example.com"
-
-}
+    "name": "Leanne"
+  },
+  {
+    "id": 2,
+    "name": "Ervin"
+  }
+]
 
 """.data(using: .utf8)!
 
 do {
-    let user = try JSONDecoder().decode(
-        User.self,
+    let users = try JSONDecoder().decode(
+        [User].self,
         from: json
     )
     
-    print(user.name)
+    if let firstuser = users.first {
+        print(firstuser.name)
+    }
+    
 } catch {
     print(error)
 }

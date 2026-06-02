38be5fb969c8a6917cda0eeada0c931be0a61c8b
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-02 07:21:13 +0700

    async/await

--- findout/BasicApi.playground/Contents.swift
+++ findout/BasicApi.playground/Contents.swift
@@ -1,3 +1,13 @@
 import Foundation
 
+let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+
+Task {
+    do {
+        
+    } catch {
+        
+    }
+}
+
 //let (data, response) = try await URLSession.shared.data(from: url)

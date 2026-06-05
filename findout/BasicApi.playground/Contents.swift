5fb025b45104cc8b058c36118b8a798947c24486
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-05 22:07:40 +0700

    a common approach production style

--- findout/BasicApi.playground/Contents.swift
+++ findout/BasicApi.playground/Contents.swift
@@ -2,8 +2,20 @@ Task {
     do {
         let users = try await fetchUsers()
         print(users)
+
+    } catch NetworkError.invalidURL {
+        print("Invalid URL")
+
+    } catch NetworkError.invalidResponse {
+        print("Invalid Response")
+
+    } catch NetworkError.badStatusCode(let code) {
+        print("Server returned \(code)")
+
+    } catch NetworkError.decodingError {
+        print("Failed to decode JSON")
+
     } catch {
-        print(error)
+        print("Unexpected error: \(error)")
     }
 }
-

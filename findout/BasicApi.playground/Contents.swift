6f9be2f109a8affe998cf0dbe47e3b634a9dad4f
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-05 22:02:48 +0700

    a common approach production style

--- findout/BasicApi.playground/Contents.swift
+++ findout/BasicApi.playground/Contents.swift
@@ -1,4 +1,20 @@
-struct User: Codable {
-    let id: Int
-    let name: String
+enum NetworkError: Error {
+    case invalideURL
+    case invalidResponse
+    case badStatusCode(Int)
+    case decodingError
+}
+
+func fetchUsers() async throws -> [User] {
+    guard let url = URL(
+        strint: "https://jsonplaceholder.typicode.com/users"
+    ) else {
+        throw NetworkError.invalidURL
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let httpsResponse = response as? HTTPURLResponse else {
+        throw NetworkError.invalidResponse
+    }
 }

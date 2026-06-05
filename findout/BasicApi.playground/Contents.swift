98092954fd438924de31c48e20ab942502ac2be1
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-05 21:57:14 +0700

    a common approach production style

--- findout/BasicApi.playground/Contents.swift
+++ findout/BasicApi.playground/Contents.swift
@@ -1,13 +1,6 @@
-import Foundation
-
-let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
-
-Task {
-    do {
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        print("Recived \(data.count) bytes")
-    } catch {
-        print(error)
-    }
+enum NetworkError: Error {
+    case invalideURL
+    case invalidResponse
+    case badStatusCode(Int)
+    case decodingError
 }

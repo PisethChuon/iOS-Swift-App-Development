5a24fc2bba5ab4a11adec3e8dc1012b25ac0b630
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-06 15:01:10 +0700

    Loading and Error Message

--- CurrencyConvertor Starter/CurrencyConvertor/Views/ContentView.swift
+++ CurrencyConvertor Starter/CurrencyConvertor/Views/ContentView.swift
@@ -11,106 +11,108 @@ struct ContentView: View {
     @State private var amount: String = ""
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Amount")
-                .font(.system(size: 15))
-            TextField("", value: $viewModel.baseAmount, formatter: viewModel.numberFormatter)
-                .font(.system(size: 18, weight: .semibold))
-                .padding()
-                .overlay {
-                    RoundedRectangle(cornerRadius: 5)
-                        .fill(.clear)
-                        .stroke(Color.gray, lineWidth: 1)
-                }
-                .overlay(alignment: .trailing) {
-                    HStack {
-                        viewModel.baseCurrency.image()
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 30)
-                            .clipShape(Circle())
-                        Menu {
-                            ForEach(CurrencyChoice.allCases) { currencyChoice in
-                                Button(action:  {
-                                    viewModel.baseCurrency = currencyChoice
-                                }, label: {
-                                    Text(currencyChoice.fetchMenuName())
-                                })
-                                
-                                
-                            }
-                        } label: {
-                            Text(viewModel.baseCurrency.rawValue)
-                                .font(.system(size: 16, weight: .bold))
-                                .foregroundStyle(.black)
-                            Image(systemName: "chevron.down")
-                                .font(Font.system(size: 16, weight: .bold))
-                                .foregroundStyle(.black)
-                        }
-                        
-                    }
-                    .padding(.trailing)
-                }
-            
-            HStack {
-                Spacer()
-                Image(systemName: "arrow.up.arrow.down")
-                    .font(.system(size: 20, weight: .bold))
-                    .padding(.vertical)
-                Spacer()
-            }
-            
-            Text("Converted To")
-            TextField("", value: $viewModel.convertedAmount, formatter: viewModel.numberFormatter)
-                .font(.system(size: 15))
-                .font(.system(size: 18, weight: .semibold))
-                .padding()
-                .overlay {
-                    RoundedRectangle(cornerRadius: 5)
-                        .fill(.clear)
-                        .stroke(Color.gray, lineWidth: 1)
-                }
-                .overlay(alignment: .trailing) {
-                    HStack {
-                        viewModel.convertedCurrency.image()
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 30)
-                            .clipShape(Circle())
-                        Menu {
-                            ForEach(CurrencyChoice.allCases) { currencyChoice in
-                                Button(action:  {
-                                    viewModel.convertedCurrency = currencyChoice
-                                }, label: {
-                                    Text(currencyChoice.fetchMenuName())
-                                })
-                                
-                                
-                            }
-                        } label: {
-                            Text(viewModel.convertedCurrency.rawValue)
-                                .font(.system(size: 16, weight: .bold))
-                                .foregroundStyle(.black)
-                            Image(systemName: "chevron.down")
-                                .font(Font.system(size: 16, weight: .bold))
-                                .foregroundStyle(.black)
-                        }
-                        
-                    }
-                    .padding(.trailing)
-                }
-            
-            HStack {
-                Spacer()
-                Text("1.00000 USD = 2.00000 EUR")
+        ZStack {
+            VStack(alignment: .leading) {
+                Text("Amount")
+                    .font(.system(size: 15))
+                TextField("", value: $viewModel.baseAmount, formatter: viewModel.numberFormatter)
                     .font(.system(size: 18, weight: .semibold))
-                    .padding(.top, 25)
-                Spacer()
+                    .padding()
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.clear)
+                            .stroke(Color.gray, lineWidth: 1)
+                    }
+                    .overlay(alignment: .trailing) {
+                        HStack {
+                            viewModel.baseCurrency.image()
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                                .clipShape(Circle())
+                            Menu {
+                                ForEach(CurrencyChoice.allCases) { currencyChoice in
+                                    Button(action:  {
+                                        viewModel.baseCurrency = currencyChoice
+                                    }, label: {
+                                        Text(currencyChoice.fetchMenuName())
+                                    })
+                                    
+                                    
+                                }
+                            } label: {
+                                Text(viewModel.baseCurrency.rawValue)
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundStyle(.black)
+                                Image(systemName: "chevron.down")
+                                    .font(Font.system(size: 16, weight: .bold))
+                                    .foregroundStyle(.black)
+                            }
+                            
+                        }
+                        .padding(.trailing)
+                    }
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "arrow.up.arrow.down")
+                        .font(.system(size: 20, weight: .bold))
+                        .padding(.vertical)
+                    Spacer()
+                }
+                
+                Text("Converted To")
+                TextField("", value: $viewModel.convertedAmount, formatter: viewModel.numberFormatter)
+                    .font(.system(size: 15))
+                    .font(.system(size: 18, weight: .semibold))
+                    .padding()
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.clear)
+                            .stroke(Color.gray, lineWidth: 1)
+                    }
+                    .overlay(alignment: .trailing) {
+                        HStack {
+                            viewModel.convertedCurrency.image()
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                                .clipShape(Circle())
+                            Menu {
+                                ForEach(CurrencyChoice.allCases) { currencyChoice in
+                                    Button(action:  {
+                                        viewModel.convertedCurrency = currencyChoice
+                                    }, label: {
+                                        Text(currencyChoice.fetchMenuName())
+                                    })
+                                    
+                                    
+                                }
+                            } label: {
+                                Text(viewModel.convertedCurrency.rawValue)
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundStyle(.black)
+                                Image(systemName: "chevron.down")
+                                    .font(Font.system(size: 16, weight: .bold))
+                                    .foregroundStyle(.black)
+                            }
+                            
+                        }
+                        .padding(.trailing)
+                    }
+                
+                HStack {
+                    Spacer()
+                    Text("1.00000 USD = 2.00000 EUR")
+                        .font(.system(size: 18, weight: .semibold))
+                        .padding(.top, 25)
+                    Spacer()
+                }
             }
-        }
-        .padding(.horizontal)
-        .task {
-            await viewModel.fetchRates()
+            .padding(.horizontal)
+            .task {
+                await viewModel.fetchRates()
+            }
         }
     }
 }

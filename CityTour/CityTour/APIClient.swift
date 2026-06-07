6e12f43df64854741355187c1c3893993dc8bd76
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-07 22:57:15 +0700

    API Client

---
+++ CityTour/CityTour/APIClient.swift
@@ -0,0 +1,14 @@
+//
+//  APIClient.swift
+//  CityTour
+//
+//  Created by chuonpiseth on 7/6/26.
+//
+
+import Foundation
+
+class APIClient {
+    private let baseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+    
+    
+}

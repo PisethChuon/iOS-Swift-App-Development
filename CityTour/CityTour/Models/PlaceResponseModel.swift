7ef8b2bfe952b8cff570e909790ec1b880049cdf
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-11 02:02:10 +0700

    Decoding JSON

---
+++ CityTour/CityTour/Models/PlaceResponseModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceResponseModel.swift
+//  CityTour
+//
+//  Created by chuonpiseth on 11/6/26.
+//
+
+import Foundation
+
+struct PlaceResponseModel: Decodable {
+    let results: [PlaceDetailResponseModel]
+}
+
+struct PlaceDetailResponseModel: Decodable {
+//    let
+}

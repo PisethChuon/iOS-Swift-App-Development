3a7593f3cc44a1d0d21f28c68792555c720ef2d1
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-11 02:27:48 +0700

    codingkey

--- CityTour/CityTour/Models/PlaceResponseModel.swift
+++ CityTour/CityTour/Models/PlaceResponseModel.swift
@@ -12,12 +12,25 @@ struct PlaceResponseModel: Decodable {
 }
 
 struct PlaceDetailResponseModel: Decodable {
+    let placeId: String
     let name: String
-    let rating: Double
-    let vicinity: String
-    let photos: [PhotoInfo]
+    let rating: Double?
+    let vicinity: String?
+    let photos: [PhotoInfo]?
+    
+    enum CodingKeys: String, CodingKey {
+        case placeId = "place_id"
+        case name
+        case rating
+        case vicinity
+        case photos
+    }
 }
 
 struct PhotoInfo: Decodable {
-    let photo_reference: String
+    let photoReference: String
+    
+    enum CodingKeys: String, CodingKey {
+        case photoReference = "photo_reference"
+    }
 }

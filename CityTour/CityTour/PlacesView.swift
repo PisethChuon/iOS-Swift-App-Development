86ab5df1ea2899eef3822be3876c8409a77b4502
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-10 07:10:25 +0700

    func fetching near by place

--- CityTour/CityTour/PlacesView.swift
+++ CityTour/CityTour/PlacesView.swift
@@ -7,6 +7,9 @@
 import SwiftUI
 
 struct PlacesView: View {
+    
+    let apiClient = APIClient()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -14,7 +17,9 @@ struct PlacesView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
-        .padding()
+        .task {
+            await apiClient.getPlaces(forKeyword: "Coffee", latitude: 40.741895, longitude: -73.989308)
+        }
     }
 }
 

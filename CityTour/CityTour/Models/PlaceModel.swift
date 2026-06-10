a92d79e548b6f026801efbc00b70877218e6dce3
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-11 01:41:14 +0700

    Place model

--- CityTour/CityTour/Models/PlaceModel.swift
+++ CityTour/CityTour/Models/PlaceModel.swift
@@ -5,3 +5,14 @@
 //  Created by chuonpiseth on 11/6/26.
 //
 
+import Foundation
+
+struct PlaceModel {
+    
+    let id: String
+    let name: String
+    let photoURL: URL
+    let rating: Double
+    let address: String
+    
+}

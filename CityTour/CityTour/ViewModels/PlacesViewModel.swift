aa0b9df8b0e20374b86320b49a7955bde95da7ec
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-13 19:08:36 +0700

    Feting User Location

--- CityTour/CityTour/ViewModels/PlacesViewModel.swift
+++ CityTour/CityTour/ViewModels/PlacesViewModel.swift
@@ -8,10 +8,17 @@
 import Foundation
 import CoreLocation
 
+// 1. Import core location
+// 2. Create a location manager
+// 3. Request permission
+// 4. Start getting location updates
+// 5. Receive the location
+
 @MainActor
 class PlacesViewModel: ObservableObject {
     
     let apiClient = APIClient()
+    private let locationManager = CLLocationManager()
     
     func fetchPlaces() {
         

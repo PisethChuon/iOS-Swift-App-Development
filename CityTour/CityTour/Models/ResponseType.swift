65f8a77e1cf0dbaf1a338421d9e24db1dd15c8da
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-13 22:30:27 +0700

    Error Handling

---
+++ CityTour/CityTour/Models/ResponseType.swift
@@ -0,0 +1,17 @@
+//
+//  ResponseType.swift
+//  CityTour
+//
+//  Created by chuonpiseth on 13/6/26.
+//
+
+import Foundation
+
+enum ResponseType {
+    case informational
+    case success
+    case redirection
+    case clientError
+    case serverError
+    case undefined
+}

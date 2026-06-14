74524c3d26db247a46b13d5f3ec8f5d1d1cd605d
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-14 12:05:10 +0700

    Result Type

---
+++ CityTour/CityTour/Models/PlacesError.swift
@@ -0,0 +1,14 @@
+//
+//  PlacesError.swift
+//  CityTour
+//
+//  Created by chuonpiseth on 14/6/26.
+//
+
+import Foundation
+
+enum PlacesError: Error {
+    case invalidURL
+    case invalidResponse
+    case apiError
+}

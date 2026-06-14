ecb8b2e7a9ddb82679d4a5404c5763faa68c405c
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-14 12:21:20 +0700

    Horizontal view

--- CityTour/CityTour/PlacesView.swift
+++ CityTour/CityTour/PlacesView.swift
@@ -11,14 +11,17 @@ struct PlacesView: View {
     @StateObject private var viewModel = PlacesViewModel()
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .task {
-//            await apiClient.getPlaces(forKeyword: "Coffee", latitude: 11.612869658013633, longitude: 104.9082483642603)
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 12) {
+                ForEach(Keyword.allCases) { keyword in
+                    Button {
+                        
+                    } label: {
+                        Text(keyword.title)
+                    }
+
+                }
+            }
         }
     }
 }

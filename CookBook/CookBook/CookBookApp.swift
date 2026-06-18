e918134feaa34f6717bae9b2c2cd575930321b3d
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-18 19:56:26 +0700

    Add CookBook

---
+++ CookBook/CookBook/CookBookApp.swift
@@ -0,0 +1,16 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}

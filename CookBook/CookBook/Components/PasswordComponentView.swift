124b31620176b9a5e1d2747cbfcbe51c4cffdfa5
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-19 00:10:25 +0700

    Password Components View

---
+++ CookBook/CookBook/Components/PasswordComponentView.swift
@@ -0,0 +1,18 @@
+//
+//  PasswordComponentView.swift
+//  CookBook
+//
+//  Created by chuonpiseth on 19/6/26.
+//
+
+import SwiftUI
+
+struct PasswordComponentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasswordComponentView()
+}

d4372a6b41a719bf05002ef441fb231977208a2e
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-19 14:17:27 +0700

    Sign Out View

--- CookBook/CookBook/Views/HomeView.swift
+++ CookBook/CookBook/Views/HomeView.swift
@@ -8,9 +8,29 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var sessionManager: SessionManager
     
+    @StateObject var viewModel = HomeViewModel()
+    
     var body: some View {
-        VStack {
-            Text("Cooking Book App!")
+        NavigationStack {
+            VStack {
+                Text("Cooking Book App!")
+            }
+            .toolbar(content: {
+                ToolbarItem {
+                    Button(action: {
+                        viewModel.showSignOutAlert = true
+                    }, label: {
+                        Image(systemName: "gearshape.fill")
+                    })
+                }
+            })
+            .alert("Are you sure you want to Sign Out?", isPresented: $viewModel.showSignOutAlert) {
+                Button("Sign Out", role: .destructive) {
+                    sessionManager.sessionState = .loggedOut
+                }
+                Button("Cancel", role: .cancel) { }
+            }
+
         }
     }
     

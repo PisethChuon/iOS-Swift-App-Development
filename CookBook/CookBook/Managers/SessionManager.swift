1bbd0176757b24a58eaccf8c1656b829fae2b4b0
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-19 12:18:30 +0700

    Session Manager

---
+++ CookBook/CookBook/Managers/SessionManager.swift
@@ -0,0 +1,12 @@
+//
+//  SessionManager.swift
+//  CookBook
+//
+//  Created by chuonpiseth on 19/6/26.
+//
+
+import Foundation
+
+class SessionManager: ObservableObject {
+    
+}
---
+++ CookBook/CookBook/Models/SessionState.swift
@@ -0,0 +1,13 @@
+//
+//  SessionState.swift
+//  CookBook
+//
+//  Created by chuonpiseth on 19/6/26.
+//
+
+import Foundation
+
+enum SessionState {
+    case loggedIn
+    case loggedOut
+}

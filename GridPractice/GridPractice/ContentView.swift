cf39c53ae1562c355a3571d7042c07162bd2b995
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-20 23:24:26 +0700

    Update

--- GridPractice/GridPractice/ContentView.swift
+++ GridPractice/GridPractice/ContentView.swift
@@ -9,16 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Grid {
-            GridRow {
-                Text("Hello")
-                Image(systemName: "globe")
-            }
-            GridRow {
-                Image(systemName: "hand.wave")
-                Text("World")
+        
+        let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+        
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(1...20, id: \.self) { number in
+                    Text("\(number)")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(.blue.opacity(0.2))
+                        .cornerRadius(8)
+                }
             }
         }
+        .padding()
     }
 }
 

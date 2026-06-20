76ef0db67b879245c11593db6c2f01598a2e58b9
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-20 20:36:27 +0700

    Practic Grid View

---
+++ GridPractice/GridPractice/GridPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridPracticeApp.swift
+//  GridPractice
+//
+//  Created by chuonpiseth on 20/6/26.
+//
+
+import SwiftUI
+
+@main
+struct GridPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

895bc3b465ba7c7097368583284d43bde33bd86f
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-20 20:38:45 +0700

    Update

--- GridPractice/GridPractice/ContentView.swift
+++ GridPractice/GridPractice/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Grid {
+            GridRow {
+                Text("Hello")
+                Image(systemName: "globe")
+            }
+            GridRow {
+                Image(systemName: "hand.wave")
+                Text("World")
+            }
         }
-        .padding()
     }
 }
 

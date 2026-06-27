58081e6f863f9fab71a2c21e8de14c77bfc455a5
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-06-28 00:06:29 +0700

    Define Coable of User

---
+++ CookBook/CookBook/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  CookBook
+//
+//  Created by chuonpiseth on 28/6/26.
+//
+
+import Foundation
+
+class User: Codable {
+    var id: Int
+    var username: String
+    var email: String
+}

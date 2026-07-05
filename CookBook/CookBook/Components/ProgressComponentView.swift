e12a00eca0558ba781f8688e0a0d0bf09ac1ddfa
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-07-05 21:55:37 +0700

    Progress Component View

---
+++ CookBook/CookBook/Components/ProgressComponentView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressComponentView.swift
+//  CookBook
+//
+//  Created by chuonpiseth on 5/7/26.
+//
+
+import SwiftUI
+
+struct ProgressComponentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressComponentView()
+}

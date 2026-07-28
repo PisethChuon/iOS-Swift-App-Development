83522c548fb950ae1280ffb482872f5d98ec7c5b
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-07-28 23:56:34 +0700

    Update PlacesView.swift

--- CityTour/CityTour/Views/PlacesView.swift
+++ CityTour/CityTour/Views/PlacesView.swift
@@ -8,6 +8,7 @@ import SwiftUI
 
 struct PlacesView: View {    
     @StateObject private var viewModel = PlacesViewModel()
+    
     private var HorizontalList: some View {
         ScrollView(.horizontal) {
             LazyHStack(spacing: 12) {

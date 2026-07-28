f965f477abd252aa9d0867fbb87d9277a8178e32
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-07-28 23:57:10 +0700

    Update PlacesViewModel.swift

--- CityTour/CityTour/ViewModels/PlacesViewModel.swift
+++ CityTour/CityTour/ViewModels/PlacesViewModel.swift
@@ -1,18 +1,8 @@
-//
-//  PlacesViewModel.swift
-//  CityTour
-//
-//  Created by chuonpiseth on 13/6/26.
-//
 
 import Foundation
 import CoreLocation
 
-// 1. Import core location
-// 2. Create a location manager
-// 3. Request permission
-// 4. Start getting location updates
-// 5. Receive the location
+// 1. Import core
 
 @MainActor
 class PlacesViewModel: NSObject, ObservableObject {

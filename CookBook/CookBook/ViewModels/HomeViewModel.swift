522295d878301745f71393d8f312d1c382468394
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-08-02 09:51:19 +0700

    Update Fetch Recipe Function

--- CookBook/CookBook/ViewModels/HomeViewModel.swift
+++ CookBook/CookBook/ViewModels/HomeViewModel.swift
@@ -13,16 +13,25 @@ import FirebaseFirestore
 class HomeViewModel {
     var showSignOutAlert: Bool = false
     var showAddRecipeView: Bool = false
-    var recipe: [Recipe] = []
+    var recipe: [Receipe] = []
     
     func fetchRecipes() async {
         guard let userId = Auth.auth().currentUser?.uid else { return }
         do {
-            let receipes = try await Firestore.firestore().collection("receipes").whereField("userId", isEqualTo: userId).getDocuments()
-                
+            let receipesResult = try await Firestore.firestore().collection("receipes").whereField("userId", isEqualTo: userId).getDocuments()
+            for receipeDocument in receipesResult.documents {
+                let data = receipeDocument.data()
+                guard let imageLocation = data["image"] as? String else { continue }
+                guard let instructions = data["instructions"] as? String else { continue }
+                guard let name = data["name"] as? String else { continue }
+                guard let time = data["time"] as? Int else { continue }
+                guard let userId = data["userId"] as? String else { continue }
+                let id = receipeDocument.documentID
+                let receipe = Receipe(id: id, name: name, image: imageLocation, instructions: instructions, time: time)
+                recipe.append(receipe)
             }
         } catch {
-            
+            print("Failed to fetch recipes: \(error.localizedDescription)")
         }
     }
     

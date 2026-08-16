50811d892e9f378432c2f8bb15c09ad21ad69232
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-08-16 11:05:35 +0700

    Findout view modifier

--- FindOut/FindOut/greeting.swift
+++ FindOut/FindOut/greeting.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+
+struct MyButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .foregroundStyle(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(.capsule)
+    }
+}
+
 struct greeting: View {
     var body: some View {
         Text("Hello, World!")
+            .modifier(MyButtonModifier())
     }
 }
 

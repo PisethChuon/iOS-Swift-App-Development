528f2f566c8984106e0b3ea8f8d528ea9f2eae0a
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-08-16 10:58:43 +0700

    View Modifier

--- FindOut/FindOut/greeting.swift
+++ FindOut/FindOut/greeting.swift
@@ -7,29 +7,8 @@
 
 import SwiftUI
 
-struct ProfileCard<Content: View>: View {
-    let name: String
-    let content: Content
-    
-    init(
-        name: String,
-        @ViewBuilder content: () -> Content
-    ) {
-        self.name = name
-        self.content = content()
-        
-    }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 12) {
-            Text(name)
-                .font(.title2)
-                .bold()
-            
-            content
-        }
-        .padding()
-        .background(.gray.opacity(0.1))
-        .clipShape(RoundedRectangle(cornerRadius: 12))
+        
     }
 }

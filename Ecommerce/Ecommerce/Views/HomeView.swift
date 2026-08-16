56e4d4eb35560bb6699ace6b22a8922cb58e0063
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-08-16 13:06:03 +0700

    Clean Code

--- Ecommerce/Ecommerce/Views/HomeView.swift
+++ Ecommerce/Ecommerce/Views/HomeView.swift
@@ -8,41 +8,37 @@ import SwiftUI
 
 struct ContentView: View {
     fileprivate var NavigationBarView: some View {
-        VStack {
-            HStack {
-                Spacer()
-                Text("Ecommerce")
-                    .font(.system(size: 16, weight: .semibold))
-                Spacer()
-            }
-            
-            .overlay(alignment: .trailing) {
-                Button(action: {
-                    
-                }, label: {
-                    ZStack {
-                        Image(systemName: "cart.fill")
-                            .foregroundStyle(.black)
-                        ZStack {
-                            Circle().fill(Color.red)
-                            Text("9")
-                                .font(.system(size: 10, weight: .bold))
-                                .foregroundColor(.white)
-                        }
-                        .offset(x: 10, y: -10)
-                    }
-                })
-            }
-            .padding(.trailing)
-            
+        
+        HStack {
+            Spacer()
+            Text("Ecommerce")
+                .font(.system(size: 16, weight: .semibold))
             Spacer()
         }
+        .overlay(alignment: .trailing) {
+            Button(action: {
+                
+            }, label: {
+                ZStack {
+                    Image(systemName: "cart.fill")
+                        .foregroundStyle(.black)
+                    ZStack {
+                        Circle().fill(Color.red)
+                        Text("9")
+                            .font(.system(size: 10, weight: .bold))
+                            .foregroundColor(.white)
+                    }
+                    .offset(x: 10, y: -10)
+                }
+            })
+        }
+        .padding(.trailing)
     }
     
     var body: some View {
         VStack {
             NavigationBarView
-            
+            Spacer()
         }
         
     }

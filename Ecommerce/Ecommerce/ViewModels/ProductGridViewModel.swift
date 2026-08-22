507d973b9b59e09258d3b59396c6d8aecfc73f36
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-08-22 13:27:56 +0700

    Define ProductGridViewModel

--- Ecommerce/Ecommerce/ViewModels/ProductGridViewModel.swift
+++ Ecommerce/Ecommerce/ViewModels/ProductGridViewModel.swift
@@ -5,3 +5,25 @@
 //  Created by chuonpiseth on 22/8/26.
 //
 
+import Foundation
+import SwiftUI
+
+@Observable
+class ProductGridViewModel {
+    let filter: ProductFilter
+    
+    var products: [Product] {
+        switch filter {
+        case .all:
+            ProductsClient.fetchProducts()
+        case .isFeatured:
+            ProductsClient.fetchProducts().filter({ $0.isFeatured })
+        case .highlyRated:
+            ProductsClient.fetchProducts().filter({ $0.rating > 4 })
+        }
+    }
+    
+    init(filter: ProductFilter) {
+        self.filter = filter
+    }
+}

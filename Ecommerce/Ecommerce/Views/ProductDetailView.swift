d6fb1ffa6464a2170190c23ea90f11f9431ebd21
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-08-22 15:43:57 +0700

    Product Detail View

---
+++ Ecommerce/Ecommerce/Views/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  Ecommerce
+//
+//  Created by chuonpiseth on 22/8/26.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}

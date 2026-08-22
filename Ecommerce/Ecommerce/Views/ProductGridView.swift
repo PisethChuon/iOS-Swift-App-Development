d0d9a73d1a9f662af042738d9fddaec550fe2b8a
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-08-22 13:34:58 +0700

    Define ProductGridViewModel

--- Ecommerce/Ecommerce/Views/ProductGridView.swift
+++ Ecommerce/Ecommerce/Views/ProductGridView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct ProductGridView: View {
+    
+    @State var viewModel: ProductGridViewModel
+    
+    init(filter: ProductFilter) {
+        self.viewModel = ProductGridViewModel(filter: filter)
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ProductGridView()
+    ProductGridView(filter: .all)
 }

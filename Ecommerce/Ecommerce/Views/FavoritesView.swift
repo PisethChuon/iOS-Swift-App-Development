6f33c567e9789b223da3ce3808d08a366dd7b00d
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-08-23 12:27:32 +0700

    FavoriteView

--- Ecommerce/Ecommerce/Views/FavoritesView.swift
+++ Ecommerce/Ecommerce/Views/FavoritesView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct FavoritesView: View {
+    
+    @State var viewModel = FavoritesViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List (viewModel.products) { product in
+                HStack {
+                    Image(product.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 70, height: 70)
+                    VStack(alignment: .leading) {
+                        Text(product.title)
+                            .font(.system(size: 15, weight: .semibold))
+                        Text(product.description)
+                            .lineLimit(2)
+                            .font(.system(size: 15))
+                            .padding(.top, 1)
+                    }
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "heart.fill")
+                            .foregroundStyle(Color.blue)
+                    })
+                }
+            }
+        }
     }
 }
 

8310e5c3b8fdf9a42133dd9f1bd713f212b1acf1
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-08-23 11:37:38 +0700

    Introducting Tab View

---
+++ Ecommerce/Ecommerce/Views/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  Ecommerce
+//
+//  Created by chuonpiseth on 23/8/26.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
---
+++ Ecommerce/Ecommerce/Views/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  Ecommerce
+//
+//  Created by chuonpiseth on 23/8/26.
+//
+
+import SwiftUI
+
+struct favoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    favoritesView()
+}

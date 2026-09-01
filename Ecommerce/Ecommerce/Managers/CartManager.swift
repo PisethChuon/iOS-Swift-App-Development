9c1b19c9509c5f3cdb447fb9f3cff35f4f9ad20e
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-01 15:10:54 +0700

    update quantity items

--- Ecommerce/Ecommerce/Managers/CartManager.swift
+++ Ecommerce/Ecommerce/Managers/CartManager.swift
@@ -13,8 +13,12 @@ class CartManager {
     var productsInCart: [ProductInCart] = []
     
     func addToCart(product: Product) {
-        if var productInCart = productsInCart.first(where: { $0.id == product.id }) {
-            productInCart.quantity += 1
+        if let indexOfProductInCart = productsInCart.firstIndex(where: { $0.id == product.id }) {
+            let currentProductInCart = productsInCart[indexOfProductInCart].quantity
+            let newQuantity = currentProductInCart + 1
+            let updatedProductInCart = ProductInCart(product: product, quantity: newQuantity)
+            productsInCart[indexOfProductInCart] = updatedProductInCart
+            
         } else {
             productsInCart.append(ProductInCart(product: product, quantity: 1))
         }

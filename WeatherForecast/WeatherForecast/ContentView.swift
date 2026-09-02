f324cbef36956332ce255ac435a3334a50024a7e
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-02 23:46:53 +0700

    use int to display temperature

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -10,23 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon")
+            DayForecast(day: "Mon", hight: 70, low: 50)
             
-            DayForecast(day: "Tue")
+            DayForecast(day: "Tue", hight: 60, low: 40)
         }
     }
 }
 
 struct DayForecast: View {
     let day: String
+    let hight: Int
+    let low: Int
     
     var body: some View {
         VStack {
             Text(day)
             Image(systemName: "sun.max.fill")
                 .foregroundStyle(Color.yellow)
-            Text("Hight: 70")
-            Text("Low: 50")
+            Text("Hight: \(hight)")
+            Text("Low: \(low)")
         }
         .padding()
     }

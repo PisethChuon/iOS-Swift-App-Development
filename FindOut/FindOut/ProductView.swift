22252b7a8bf82c47642c3e790d916c9502eb314a
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-02 15:59:10 +0700

    Find out

--- FindOut/FindOut/ProductView.swift
+++ FindOut/FindOut/ProductView.swift
@@ -9,26 +9,39 @@ import SwiftUI
 
 struct ProductView: View {
     
-    @Environment(FavoritesStore.self) private var favoritesStore
+    //    @Environment(FavoritesStore.self) private var favoritesStore
+    
+    //    var body: some View {
+    //        List {
+    //            ForEach(products) { product in
+    //                HStack {
+    //                    Text(product.name)
+    //                    Spacer()
+    //                    Button {
+    //                        favoritesStore.toggleFavorite(product)
+    //                    } label: {
+    //                        Image(systemName: favoritesStore.isFavorite(product) ? "heart.fill" : "heart")
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
     
     var body: some View {
-        List {
-            ForEach(products) { product in
-                HStack {
-                    Text(product.name)
-                    Spacer()
-                    Button {
-                        favoritesStore.toggleFavorite(product)
-                    } label: {
-                        Image(systemName: favoritesStore.isFavorite(product) ? "heart.fill" : "heart")
-                    }
-                }
-            }
+        VStack {
+            Text("Knock, knock!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+            Text("Who's there?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
         }
+        .padding()
     }
+    
 }
 
 #Preview {
     ProductView()
-        .environment(FavoritesStore())
+    //        .environment(FavoritesStore())
 }

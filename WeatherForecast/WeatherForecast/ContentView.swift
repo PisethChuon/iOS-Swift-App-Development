216490513f93c9a28f8a76cf49a7aa5acdabe7c6
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-02 23:12:36 +0700

    add other weatherforcase

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Mon")
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
-            Text("Hight: 70")
-            Text("Low: 50")
+        HStack {
+            VStack {
+                Text("Mon")
+                Image(systemName: "sun.max.fill")
+                    .foregroundStyle(Color.yellow)
+                Text("Hight: 70")
+                Text("Low: 50")
+            }
+            .padding()
+            
+            VStack {
+                Text("Mon")
+                Image(systemName: "sun.max.fill")
+                    .foregroundStyle(Color.yellow)
+                Text("Hight: 60")
+                Text("Low: 40")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 

5d14f1169e019fcc536c32520b216f9e6c437429
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-02 23:10:03 +0700

    Weatherforecase

---
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WeatherForecast
+//
+//  Created by chuonpiseth on 2/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Mon")
+            Image(systemName: "sun.max.fill")
+                .foregroundStyle(Color.yellow)
+            Text("Hight: 70")
+            Text("Low: 50")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by chuonpiseth on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

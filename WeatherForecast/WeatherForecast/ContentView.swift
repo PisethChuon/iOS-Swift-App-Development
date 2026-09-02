0f2b5641b5caa96ba4ecd3b92a06cca92cd05c00
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-02 23:23:29 +0700

    custome view

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -11,23 +11,27 @@ struct ContentView: View {
     var body: some View {
         HStack {
             VStack {
-                Text("Mon")
-                Image(systemName: "sun.max.fill")
-                    .foregroundStyle(Color.yellow)
-                Text("Hight: 70")
-                Text("Low: 50")
-            }
-            .padding()
-            
-            VStack {
-                Text("Mon")
-                Image(systemName: "sun.max.fill")
-                    .foregroundStyle(Color.yellow)
+                Text("Tue")
+                Image(systemName: "cloud.rain.fill")
+                    .foregroundStyle(Color.blue)
                 Text("Hight: 60")
                 Text("Low: 40")
             }
             .padding()
         }
+    }
+}
+
+struct DayForecast: View {
+    var body: some View {
+        VStack {
+            Text("Mon")
+            Image(systemName: "sun.max.fill")
+                .foregroundStyle(Color.yellow)
+            Text("Hight: 70")
+            Text("Low: 50")
+        }
+        .padding()
     }
 }
 

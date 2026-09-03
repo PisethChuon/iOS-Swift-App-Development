80786ee687b02fc553eb810b84306e91acb431d6
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-03 23:41:32 +0700

    DiceRoller

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by chuonpiseth on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

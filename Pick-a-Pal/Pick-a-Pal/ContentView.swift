963fa1565024735bfa6dfcce53d1c32c6e30c741
Piseth Chuon <122347263+PisethChuon@users.noreply.github.com>
2026-09-04 19:08:28 +0700

    improve-app-design

--- Pick-a-Pal/Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/Pick-a-Pal/ContentView.swift
@@ -8,20 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var names: [String] = ["Elisha", "Andre", "Jasmin", "Po-Chum"]
+    @State private var names: [String] = []
     @State private var nameToAdd = ""
     @State private var pickedName = ""
     @State private var shouldRemovePickedName = false
     
     var body: some View {
         VStack {
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Pick-a-Pal")
+            }
+            .font(.title)
+            .bold()
+            
             Text(pickedName.isEmpty ? " " : pickedName)
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.tint)
             
             List {
                 ForEach(names, id: \.description) { name in
                     Text(name)
                 }
             }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
             
             TextField("Add Name", text: $nameToAdd)
                 .autocorrectionDisabled()
